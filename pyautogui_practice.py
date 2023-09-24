4c3914bb014961bae6833771418253ff5a9a29e0
jahangir842 <jahangir80842@gmail.com>
2023-09-24 18:00:44 +0500

    spiral Feature_branch

--- pyautogui_practice.py
+++ pyautogui_practice.py
@@ -1,5 +1,18 @@
 import pyautogui
 import time
 
-print(pyautogui.size())
-print(pyautogui.position())
+# time.sleep(3.5)
+
+# print(pyautogui.size())
+# print(pyautogui.position())
+
+# pyautogui.moveTo(100, 300, 1)
+time.sleep(3)
+distance = 300
+while distance > 0:
+    pyautogui.dragRel(distance, 0, 1, button="left")
+    distance -= 25
+    pyautogui.dragRel(0, distance, 1, button= "left")
+    pyautogui.dragRel(-distance, 0, 1, button= "left")
+    distance -= 25
+    pyautogui.dragRel(0, -distance, 1, button= "left")
